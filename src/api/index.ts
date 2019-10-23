6dd8f5c8cff600215815be5586965820089d7d82
Ricardo Casares <ricardocasares@gmail.com>
2019-10-23 21:44:04 +0200

    remove unused API

--- src/api/index.ts
+++
@@ -1,8 +0,0 @@
-import { encode } from "querystring";
-import fetch from "isomorphic-unfetch";
-
-const API = process.env.API;
-const KEY = process.env.API_KEY;
-
-export const api = (params: Record<string, string> = {}) =>
-  fetch(`${API}?${encode({ ...params, access_key: KEY })}`).then(r => r.json());
